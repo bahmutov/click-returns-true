09460850b497c220c2526bf75fd783ae443ec9d9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-05-04 21:10:48 -0400

    add disabled button test

--- cypress/component/click.spec.ct.js
+++ cypress/component/click.spec.ct.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { mount } from '@cypress/react'
 
-const Button = ({ onClick }) => <button onClick={onClick}>Click me</button>
+const Button = ({ onClick, disabled }) => (
+  <button disabled={disabled} onClick={onClick}>
+    Click me
+  </button>
+)
 
 it('renders an active base button', () => {
   mount(<Button onClick={cy.stub().as('click')}>Click me</Button>)
@@ -10,4 +14,15 @@ it('renders an active base button', () => {
   cy.get('@click').should('have.been.calledOnce').invoke('resetHistory')
   cy.get('button').click().click()
   cy.get('@click').should('have.been.calledTwice')
+})
+
+it('does not call the handler on the disabled button', () => {
+  mount(
+    <Button disabled={true} onClick={cy.stub().as('click')}>
+      Click me
+    </Button>,
+  )
+
+  cy.get('button').click({ force: true })
+  cy.get('@click').should('not.have.been.called')
 })
